a1071c76ba9b96801890a611d9598b67a9c89deb
Himesh-Maurya <himeshmauurya@gmail.com>
2023-02-08 07:13:23 +0530

    fetching api making of render page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { fetchWeather } from "./fetchWeather";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [find, setfind] = useState("");
+  const [weather, setWeather] = useState("");
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(find);
+      setWeather(data);
+      setfind("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <input
+        type="text"
+        placeholder="Search your city..."
+        className="search"
+        value={find}
+        onChange={(e) => setfind(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp - 273.15)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
